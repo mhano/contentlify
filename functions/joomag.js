93587d3d63828a7d1e161724645e087b6410c829
Mhano Harkness <28881779+mhano@users.noreply.github.com>
2019-04-04 15:21:22 +1100

    test

--- functions/joomag.js
+++ functions/joomag.js
@@ -1,26 +1,14 @@
 import fetch from "node-fetch";
-import sha256 from "js-sha256";
 
-const { JOOMAG_API_ENDPOINT } = process.env;
-const { JOOMAG_API_ID } = process.env;
-const { JOOMAG_API_SECRET } = process.env;
+const API_ENDPOINT =
+  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
 
 exports.handler = async (event, context) => {
-  
-  var pubid = event.queryStringParameters.pubid; // M0805154001554340774
-  var apiEndpoint = API_ENDPOINT + "/magazines/" + pubid + "/issues"
-  var sigInput = "GET" + url;
-  var sigHmac = sha256.hmac(JOOMAG_API_SECRET, query);
-  
-  return fetch(apiEndpoint, {
-	  key: JOOMAG_API_ID,
-	  sig: sigHmac
-	})
+  return fetch(API_ENDPOINT)
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-	  contentType: "application/vnd.cpu.republivision.v1+json",
-      body: JSON.stringify(data)
+      body: `${data.setup} ${data.punchline} *BA DUM TSSS*`
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
