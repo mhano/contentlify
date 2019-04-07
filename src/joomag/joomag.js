eb3bd0d22f5f5fdc303c2b2eb205dcc122015c49
Mhano Harkness <28881779+mhano@users.noreply.github.com>
2019-04-08 00:31:21 +1000

    updated joomag function to include nodejs lambda based memory caching

--- src/joomag/joomag.js
+++ src/joomag/joomag.js
@@ -5,8 +5,39 @@ const { JOOMAG_API_ENDPOINT } = process.env;
 const { JOOMAG_API_ID } = process.env;
 const { JOOMAG_API_SECRET } = process.env;
 
+// cache mechanism assumes all cache items stored with same timeout/max age
+// this makes the cache removal very efficient, can find find all items that
+// need removing and then stop.
+var lastCacheCleanup = Date.now();
+var maxCacheCleanupInterval = 30000;
+var cacheMaxAgeMs = 30000;
+var cache = {};
+var cacheByAge = new Array();
+async function removeOldCacheEntries(maxAge) {
+	// only do a cache cleanup if we haven't in 
+	if(lastCacheCleanup < (Date.now() - maxCacheCleanupInterval)) {
+		lastCacheCleanup = Date.now();
+		
+		var deleteCount = 0;
+		for(var i = 0; i< cacheByAge.length && cacheByAge[i].ts < maxAge; i ++) {
+		  deleteCount++;
+		}
+
+		if(deleteCount > 0) {
+			for(var i = 0; i < deleteCount; i++) {
+				delete cache[cacheByAge[i].key];
+			}
+			cacheByAge.splice(0, deleteCount);
+		}
+		
+		console.log(`Cleared ${deleteCount} cache items.`);
+	}
+}
+
 exports.handler = async (event, context) => {
   var startDate = new Date();
+  
+  var maxAge = Date.now() - cacheMaxAgeMs;
   
   var regex = /^[A-Za-z0-9]{5,100}$/;
   
@@ -16,14 +47,39 @@ exports.handler = async (event, context) => {
 	  throw "pubid must be ^[a-zA-Z0-9]{5,100}$";
   }
   
+  var cresult = cache[pubid];
+  
+  // 30 second cache
+  if(cresult && cresult.ts && cresult.ts > maxAge) {
+	console.log(`Cache hit: ${pubid}`);
+	var result = 
+	{
+	  statusCode: 200,
+	  headers: {
+		  "Content-Type": "application/vnd.cpu.republivision.v1+json",
+		  "Access-Control-Allow-Origin": "*",
+		  "x-joomag-cache-status": "hit"
+	  },
+	  body: cresult.data
+	};
+	
+	return result;
+  } else {
+	console.log(`Cache miss: ${pubid}`);
+  }
+  
   var apiEndpoint = JOOMAG_API_ENDPOINT + "/magazines/" + pubid + "/issues"
   var sigInput = "GET" + apiEndpoint;
   var sigHmac = sha256.hmac(JOOMAG_API_SECRET, sigInput);
   
   var start = Date.now();
   
-  return fetch(apiEndpoint, {headers: {key: JOOMAG_API_ID, sig: sigHmac }})
-    .then(response => response.json())
+  // in parallel fetch latest and clear out old cache items
+  return Promise.all([
+	fetch(apiEndpoint, {headers: {key: JOOMAG_API_ID, sig: sigHmac }}),
+	removeOldCacheEntries(maxAge)
+  ])
+	.then(response => response[0].json())
     .then(function(data){
 		var responseJson = JSON.stringify(data.data);
 		
@@ -43,10 +99,15 @@ exports.handler = async (event, context) => {
 		  statusCode: 200,
 		  headers: {
 			  "Content-Type": "application/vnd.cpu.republivision.v1+json",
-			  "Access-Control-Allow-Origin": "*"
+			  "Access-Control-Allow-Origin": "*",
+			  "x-joomag-cache-status": "miss"
 		  },
 		  body: responseJson
 		};
+		
+		var cacheItem = {ts: Date.now(), key: pubid, data: responseJson};
+		cache[pubid] = cacheItem;
+		cacheByAge.push(cacheItem);
 		
 		return result;
 	})
