8cc4b9a764eefafedc169760f3152e4cfaf2414e
Mhano Harkness <28881779+mhano@users.noreply.github.com>
2019-04-08 09:48:51 +1000

    Create test2.js

---
+++ functions/test2.js
@@ -0,0 +1,10 @@
+
+exports.handler = async (event, context) => {
+	return {
+	  statusCode: 200,
+	  headers: {
+		  "Content-Type": "application/json"
+	  },
+	  body: JSON.stringify(event);
+	};
+}
